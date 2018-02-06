690180d4a5a2e6758f85360d0a8f180e59bfff9f
David <david@evason.co.za>
2018-02-06 19:45:30 +0200

    Update templates to include .ate* files

---
+++ templates/CSharpPocoSingle/ʕAppℴDisplayNameʔ Models.cs
@@ -0,0 +1,21 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ʕAppℴFullNameʔ
+{
+
+    //ʕfor each Entity in AppℴEntitiesʔ
+    public class ʕEntityℴNameʔ
+    {
+
+        //ʕfor each Property in EntityℴPropertiesʔ
+        public ʕPropertyℴCSharp()ʔ ʕPropertyℴNameʔ { get; set; }
+        //ʕnext Propertyʔ
+
+    }
+
+    //ʕnext Entityʔ
+}
